8ff82ae7d3cc2a0b6361b0df6536948e220cedf1
unknown <bprince@ws-bprince.ccs.local>
2014-05-02 16:23:39 -0400

    Added tests to create a hand and to for one pair

--- pokerHandScorer.js
+++ pokerHandScorer.js
@@ -1,5 +1,40 @@
 function pokerHandScorer() {
-	this.scoreHand = function(card) {
+	this.scoreHand = function(cards) {
+		var hand = this.createHand(cards);
+	
+		if (isPair(hand))
+			return "One Pair";
+		
 		return "High Card";
 	}
+	
+	this.createHand = function(rawCards) {
+		var cards = rawCards.split(" ");
+		var hand = new Array(5);
+		
+		for(var i = 0; i < cards.length; i++) {
+			hand[i] = new card();
+			hand[i].rank = cards[i].substring(0, 1);
+			hand[i].suit = cards[i].substring(1, 2);
+		}
+		
+		return hand;
+	}
+	
+	function isPair(hand) {
+		for(var i = 0; i < hand.length; i++) {
+			var card = hand[i];
+			
+			for(var j = 0; j < hand.length; j++) {
+				if (i != j)
+					if (card.rank == hand[j].rank)
+						return true;
+			}
+		}
+	}
+}
+
+function card() {
+	this.suit = "";
+	this.rank = 0;
 }
--- pokerHandTests.js
+++ pokerHandTests.js
@@ -1,4 +1,20 @@
-test( "An uninteresting hand just returns High Card", function() {
+test( "1C 3H 5D 6S 8H just returns High Card", function() {
   var scorer = new pokerHandScorer();
   equal( scorer.scoreHand("1C 3H 5D 6S 8H"), "High Card");
+});
+
+test( "8C 3H 5D 6S 8H just has a first element with rank 8", function() {
+  var scorer = new pokerHandScorer();
+  var hand = scorer.createHand("8C 3H 5D 6S 8H");
+  equal( hand[0].rank, 8);
+});
+
+test( "8C 3H 5D 6S 8H just returns One pair", function() {
+  var scorer = new pokerHandScorer();
+  equal( scorer.scoreHand("8C 3H 5D 6S 8H"), "One Pair");
+});
+
+test( "8C 3H 3D 6S 8H just returns two pair", function() {
+  var scorer = new pokerHandScorer();
+  equal( scorer.scoreHand("8C 3H 3D 6S 8H"), "Two Pair");
 });
