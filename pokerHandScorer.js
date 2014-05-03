57d56347717f65e1c7b8d3b5be51e72d30789b95
neoprincie <bryprinc@gmail.com>
2014-05-02 22:10:18 -0400

    Added test for two pair

--- pokerHandScorer.js
+++ pokerHandScorer.js
@@ -4,7 +4,9 @@ function pokerHandScorer() {
 	this.scoreHand = function(cards) {
 		hand = this.createHand(cards);
 	
-		if (isPair(hand))
+		if (isTwoPair())
+		    return "Two Pair";
+		if (isPair())
 			return "One Pair";
 		
 		return "High Card";
@@ -23,31 +25,42 @@ function pokerHandScorer() {
 		return hand;
 	}
 	
-	function isPair(hand) {
+	function isPair() {
 		for(var i = 0; i < hand.length; i++) {
-		    if (GetNumberOfMatches(hand[i]) > 0)
+		    if (getNumberOfMatches(hand[i]) == 1)
 		        return true;
 		}
 
 		return false;
 	}
 
-	function GetNumberOfMatches(card) {
+	function isTwoPair() {
+	    var numberOfPairs = 0;
+
+	    for (var i = 0; i < hand.length; i++) {
+	        if (getNumberOfMatches(hand[i]) == 1)
+	            numberOfPairs++;
+	    }
+
+	    return numberOfPairs / 2 == 2;
+	}
+
+	function getNumberOfMatches(card) {
 	    var matches = 0;
 
 	    for (var i = 0; i < hand.length; i++) {
-	        if (CardsAreNotTheSame(card, hand[i]) && CardsAreTheSameRank(card, hand[i]))
+	        if (cardsAreNotTheSame(card, hand[i]) && cardsAreTheSameRank(card, hand[i]))
 	            matches++;
 	    }
 
 	    return matches;
 	}
 
-	function CardsAreNotTheSame(card1, card2) {
+	function cardsAreNotTheSame(card1, card2) {
 	    return card1.rank != card2.rank || card1.suit != card2.suit
 	}
 
-	function CardsAreTheSameRank(card1, card2) {
+	function cardsAreTheSameRank(card1, card2) {
 	    return card1.rank == card2.rank;
 	}
 }
