80f4542a819b7170ae727d3086ed80b9ddc5d4d4
bprince <bprince@foxguardsolutions.com>
2014-05-16 14:46:38 -0400

    Refactored two pair, and added three of a kind and straight

--- pokerHandScorer.js
+++ pokerHandScorer.js
@@ -4,6 +4,10 @@ function pokerHandScorer() {
 	this.scoreHand = function(cards) {
 		hand = this.createHand(cards);
 	
+		if (isStraight())
+		    return "Straight";
+		if (isThreeOfAKind())
+		    return "Three of a Kind";
 		if (isTwoPair())
 		    return "Two Pair";
 		if (isPair())
@@ -13,10 +17,10 @@ function pokerHandScorer() {
 	}
 	
 	this.createHand = function(rawCards) {
-		var cards = rawCards.split(" ");
-		var hand = new Array(5);
+	    var cards = rawCards.split(" ");
+	    var hand = new Array(5);
 		
-		for(var i = 0; i < cards.length; i++) {
+	    for (var i = 0; i < cards.length; i++) {
 			hand[i] = new card();
 			hand[i].rank = cards[i].substring(0, 1);
 			hand[i].suit = cards[i].substring(1, 2);
@@ -27,7 +31,7 @@ function pokerHandScorer() {
 	
 	function isPair() {
 		for(var i = 0; i < hand.length; i++) {
-		    if (getNumberOfMatches(hand[i]) == 1)
+		    if (getNumberOfMatches(hand[i], i) == 1)
 		        return true;
 		}
 
@@ -38,20 +42,37 @@ function pokerHandScorer() {
 	    var numberOfPairs = 0;
 
 	    for (var i = 0; i < hand.length; i++) {
-	        if (getNumberOfMatches(hand[i]) == 1)
+	        if (getNumberOfMatches(hand[i], i) == 1)
 	            numberOfPairs++;
 	    }
 
-	    return numberOfPairs / 2 == 2;
+	    return numberOfPairs == 2;
 	}
 
-	function getNumberOfMatches(card) {
+	function isThreeOfAKind() {
+	    for (var i = 0; i < hand.length; i++) {
+	        if (getNumberOfMatches(hand[i], i) == 2)
+	            return true;
+	    }
+
+	    return false;
+	}
+
+	function isStraight() {
+	    hand.sort(function (a, b) { return parseFloat(a.rank) - parseFloat(b.rank) });
+
+	    if (hand[0].rank == hand[1].rank - 1 == hand[2].rank - 2 == hand[3].rank - 3 == hand[4].rank - 4)
+	        return true;
+
+	    return false;
+	}
+
+	function getNumberOfMatches(card, startIndex) {
 	    var matches = 0;
 
-	    for (var i = 0; i < hand.length; i++) {
+	    for (var i = startIndex; i < hand.length; i++)
 	        if (cardsAreNotTheSame(card, hand[i]) && cardsAreTheSameRank(card, hand[i]))
 	            matches++;
-	    }
 
 	    return matches;
 	}
--- pokerHandTests.js
+++ pokerHandTests.js
@@ -18,3 +18,13 @@ test( "8C 3H 3D 6S 8H just returns two pair", function() {
   var scorer = new pokerHandScorer();
   equal( scorer.scoreHand("8C 3H 3D 6S 8H"), "Two Pair");
 });
+
+test("8C 3H 8D 6S 8H just returns three of a kind", function () {
+    var scorer = new pokerHandScorer();
+    equal(scorer.scoreHand("8C 3H 8D 6S 8H"), "Three of a Kind");
+});
+
+test("3C 2H 1D 4H 5D just returns straight", function () {
+    var scorer = new pokerHandScorer();
+    equal(scorer.scoreHand("3C 2H 1D 4H 5D"), "Straight");
+});
